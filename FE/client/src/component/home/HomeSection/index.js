1d2231dd249d77198b3e7a625d13eaf7f42abd71
thaind-2785 <nguyen.duy.thai@sun-asterisk.com>
2022-03-11 22:34:51 +0700

    home section

--- FE/client/src/component/home/HomeSection/index.js
+++ FE/client/src/component/home/HomeSection/index.js
@@ -1,28 +1,41 @@
 import './index.scss'
 import ProductItem from '../../products/ProductItem'
-import { Carousel } from '@trendyol-js/react-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function HomeSection(props) {
     const { list, leftImage, headerText } = props
-    console.log(list)
+    const setting = {
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        infinite: false,
+        dots: false,
+        arrows: true
+    };
+
     return ( 
         <section className="home__section">
             <div className='home__section__header flex alignItem-center'>
                 <i className="fa-solid fa-angles-right"></i>
                 <h1>{headerText}</h1>
             </div>
-            <div className='home__section__content flex alignItem-center justify-center'>
+            <div className='home__section__content flex alignItem-start justify-center'>
                 <a className='home__section__leftImgage'>
                     <img className='home__section__leftImgage__img' src={leftImage}/>
                 </a>
                 <div className='home__section__listDemo'>
-                    <Carousel>
-                        {
-                            list.length 
-                            ? list.map(item => <ProductItem data={item} key={item.id}/>)
-                            : <Loading/>
-                        }
-                    </Carousel>
+                    {list.length &&
+                        <Slider {...setting}>
+                            {list.map((item,index) => <ProductItem data={item} key={item.id}/>)}
+                        </Slider>
+                    // list.map(item => {return (<ProductItem data={item} key={item.id}/>)})
+                    
+                    
+                    
+                    }
                 </div>    
             </div>
         </section>
